0d6da88700710f838fad66f012b21fcc117924b2
cercinc <caglar.cercinli@vrt.be>
2022-02-13 15:26:19 +0100

    migrated to nodemon

--- index.js
+++ index.js
@@ -1,52 +1,21 @@
 const { ApolloServer } = require('apollo-server');
-const { gql } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const Post = require('./models/Post');
-const User = require('./models/User');
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type User {
-    id: ID!
-    email: String!
-    token: String!
-    username: String!
-    createdAt: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-const resolvers = {
-    Query: {
-        async getPosts() {
-            try {
-                const posts = await Post.find();
-                return posts;
-            } catch (err){
-                throw new Error(err);
-            }
-       }
-    }
-};
 
 const server = new ApolloServer({
         typeDefs,
         resolvers
     });
 
-    mongoose.connect(MONGODB, { useNewUrlParser: true })
-        .then(() => {
-            console.log('mongodb connected');
-            return server.listen({ port: 4000 });
-        })
-        .then(res => {
-            console.log(`Server running at ${res.url}`);
-        });
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log('mongodb connected');
+        return server.listen({ port: 4000 });
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}`);
+    });

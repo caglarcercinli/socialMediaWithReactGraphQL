ef6d401f886928ab683cce5e4e946648ec4644e0
cercinc <caglar.cercinli@vrt.be>
2022-02-20 15:17:33 +0100

    create/delete posts

---
+++ util/check-auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const {SECRET_KEY} = require('../config');
+
+const {AuthenticationError} = require("apollo-server");
+
+module.exports = (context) => {
+    const authHeader = context.req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split('Bearer ')[1];
+        if (token) {
+            try {
+                const user = jwt.verify(token, SECRET_KEY);
+                return user;
+            } catch (e) {
+                throw new AuthenticationError('invalid/ expired token');
+            }
+        }
+        throw new Error("authentication token must be 'Bearer [token]");
+    }
+    throw new Error('authentication header must be provided');
+};
